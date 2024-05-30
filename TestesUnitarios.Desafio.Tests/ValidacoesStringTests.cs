68c2e1bb9fb655ebbd779b4cded3ba5b34c7cdab
Matheus Madureira <matheus_mmadu@hotmail.com>
2024-05-30 17:16:43 -0300

    Ultimo FACT

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -59,6 +59,7 @@ public class ValidacoesStringTests
     }
 
     //TODO: Corrigir a anotação [Fact]
+    [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
         //TODO: Corrigir a variável "textoProcurado" seção Arrange
